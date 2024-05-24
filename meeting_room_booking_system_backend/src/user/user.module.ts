02ad8630ab0ab983b21fc3984076625e1c671234
hqy <469346971@qq.com>
2024-05-24 10:04:08 +0800

    登录和权限的鉴权，自定义装饰器

--- meeting_room_booking_system_backend/src/user/user.module.ts
+++ meeting_room_booking_system_backend/src/user/user.module.ts
@@ -3,10 +3,12 @@ import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user.entity";
+import { Role } from "./entities/role.entity";
+import { Permission } from "./entities/permissions.entity";
 
 @Module({
   // 实体与模块关联
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Role, Permission])],
   controllers: [UserController],
   providers: [UserService],
 })
